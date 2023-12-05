ce50404196e24cf3edbf678a482dea03998a6933
Ben Hulse <bhulse@beyondtrust.com>
2023-12-05 15:11:28 +0000

    Day 5 - Part 2

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -35,6 +35,16 @@ impl Almanac {
         }
         dest
     }
+
+    fn get_location_range(&self, ranges: Vec<NumRange>) -> Vec<NumRange> {
+        let mut seed_ranges = ranges;
+
+        for map_set in &self.map_sets {
+            seed_ranges = map_set.apply_to_range(seed_ranges);
+        }
+
+        seed_ranges
+    }
 }
 
 #[derive(Debug)]
@@ -78,6 +88,25 @@ impl MappingSet {
             .collect();
         
         *dests.first().unwrap_or(&input)
+    }
+
+    fn apply_to_range(&self, input_ranges: Vec<NumRange>) -> Vec<NumRange> {
+        let mut ranges = input_ranges;
+        let mut modified_ranges = Vec::new();
+        for mapping in &self.mappings {
+            // Each number can only be modified by each MappingSet once
+            // Keep ranges of numbers that have changed separate so that they're not processed by later mappings
+            let mut new_modified_ranges;
+            (ranges, new_modified_ranges) = mapping.apply_to_ranges(ranges);
+
+            if new_modified_ranges.len() > 0 {
+                modified_ranges.append(&mut new_modified_ranges);
+            }
+        }
+
+        // Add the modified ranges back once the MappingSet is finished
+        ranges.append(&mut modified_ranges);
+        ranges
     }
 }
 
@@ -124,6 +153,87 @@ impl Mapping {
             input
         }
     }
+
+    fn apply_to_ranges(&self, input_ranges: Vec<NumRange>) -> (Vec<NumRange>, Vec<NumRange>) {
+        let mut unmodified = Vec::new();
+        let mut modified = Vec::new();
+        for seed_range in input_ranges {
+            let ranges: (Option<NumRange>, Option<NumRange>, Option<NumRange>) = seed_range.apply_mapping(self);
+
+            if let Some(ul) = ranges.0 {
+                unmodified.push(ul);
+            }
+            if let Some(ur) = ranges.1 {
+                unmodified.push(ur);
+            }
+            if let Some(m) = ranges.2 {
+                modified.push(m);
+            }
+        }
+
+        (unmodified, modified)
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct NumRange {
+    start: i64,
+    len: i64
+}
+
+impl NumRange {
+    fn apply_mapping(&self, mapping: &Mapping) -> (Option<NumRange>, Option<NumRange>, Option<NumRange>) {
+        let old_left = self.start;
+        let old_right = self.start + self.len - 1;
+
+        let mapping_left = mapping.source;
+        let mapping_right = mapping.source + mapping.range - 1;
+
+        // Case where original is entirely inside mapping
+        if mapping_left <= old_left && old_right <= mapping_right {
+            return (None, None , Some(
+                NumRange { 
+                    start: self.start+mapping.offset, 
+                    len: self.len 
+                }));
+        }
+
+        let new_left = if old_left <= mapping_left && mapping_left <= old_right {mapping_left} else {old_left};
+        let new_right = if old_left <= mapping_right && mapping_right <= old_right {mapping_right} else {old_right};
+
+        // No mapping applied
+        if new_left == old_left && new_right == old_right {
+            return (Some(*self), None, None);
+        }
+
+        let unmodified_left = if new_left > old_left {
+            Some(NumRange { 
+                    start: old_left, 
+                    len: new_left - old_left
+                })
+        }
+        else {
+            None
+        };
+
+        let unmodified_right = if new_right < old_right {
+            Some(NumRange { 
+                    start: new_right + 1, 
+                    len: old_right - new_right 
+                })
+        } 
+        else {
+            None
+        };
+
+        let modified_middle = Some(
+            NumRange { 
+                start: new_left + mapping.offset, 
+                len: new_right - new_left + 1 
+            });
+
+        (unmodified_left, unmodified_right, modified_middle)
+    }
 }
 
 pub fn part1() -> i64 {
@@ -147,13 +257,25 @@ pub fn part2() -> i64 {
 
     let seed_nums : Vec<_> = seeds_str.split_ascii_whitespace().filter_map(|s| s.parse::<i64>().ok()).collect();
 
-    // Brute force does not work
+    let mut seed_ranges = Vec::new();
 
-    println!("{:?}",seeds.len());
+    for seed_start_i in (0..seed_nums.len()).step_by(2) {
+        let seed_start = seed_nums[seed_start_i];
+        let seed_range = seed_nums[seed_start_i + 1]; 
+
+        seed_ranges.push(
+            NumRange {
+                start: seed_start,
+                len: seed_range
+            }
+        )
+    }
     
     let almanac = Almanac::from_str(maps_str).unwrap();
 
-    let min_location = seeds.iter().map(|&s| almanac.get_location(s)).min();
+    let loc_ranges = almanac.get_location_range(seed_ranges);
 
-    min_location.unwrap_or(0)
+    let min_loc = loc_ranges.into_iter().min_by_key(|r| r.start);
+
+    min_loc.unwrap().start
 }
