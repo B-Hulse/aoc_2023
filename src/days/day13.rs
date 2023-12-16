7bb6caf9d1c40724c0953822266ed02a53af24ff
Ben Hulse <b.hulse99@gmail.com>
2023-12-16 18:01:23 +0000

    Day 13 - Part 2

--- src/days/day13.rs
+++ src/days/day13.rs
@@ -21,6 +21,13 @@ fn find_vertical_mirror(input: &Vec<Vec<char>>) -> Option<i64> {
     find_horizontal_mirror(&transposed_input)
 }
 
+fn find_vertical_mirror_one_off(input: &Vec<Vec<char>>) -> Option<i64> {
+    // Switch the rows and the columns in the input
+    let transposed_input = transpose(input.clone());
+
+    find_horizontal_mirror_one_off(&transposed_input)
+}
+
 fn find_horizontal_mirror(input: &Vec<Vec<char>>) -> Option<i64> {
     
     for i in 1..input.len() {
@@ -39,6 +46,41 @@ fn find_horizontal_mirror(input: &Vec<Vec<char>>) -> Option<i64> {
             }
         }
         if mirrored {
+            return Some(i as i64);
+        }
+    }
+
+    None
+}
+
+fn find_horizontal_mirror_one_off(input: &Vec<Vec<char>>) -> Option<i64> {
+    for i in 1..input.len() {
+        let mut up_rows = (&input[0..i]).to_vec();
+        up_rows.reverse();
+        let down_rows = (&input[i..input.len()]).to_vec();
+
+        let min_row_i = up_rows.len().min(down_rows.len());
+
+        let mut flaws = 0;
+
+        for i in 0..min_row_i {
+            if up_rows[i] != down_rows[i] {
+                for j in 0..up_rows[i].len() {
+                    let up_rows_j = up_rows[i][j];
+                    let down_rows_j = down_rows[i][j];
+
+                    if up_rows_j != down_rows_j {
+                        flaws+=1;
+                    }
+                }
+
+                if flaws > 1 {
+                    break;
+                }
+            }
+        }
+
+        if flaws == 1 {
             return Some(i as i64);
         }
     }
@@ -72,5 +114,26 @@ pub fn part1() -> i64 {
 }
 
 pub fn part2() -> i64 {
-    0
+    let patterns: Vec<_> = INPUT.split("\r\n\r\n").collect();
+
+    let patterns: Vec<_> = patterns
+        .iter()
+        .map(|p| {
+            p.lines()
+                .map(|l| l.chars().collect::<Vec<_>>())
+                .collect::<Vec<_>>()
+        })
+        .collect();
+
+    let vertical_mirror_sums: i64 = patterns
+        .iter()
+        .filter_map(|p| find_vertical_mirror_one_off(p))
+        .sum();
+
+    let horizontal_mirror_sums: i64 = patterns
+        .iter()
+        .filter_map(|p| find_horizontal_mirror_one_off(p))
+        .sum();
+    
+    vertical_mirror_sums + (horizontal_mirror_sums * 100) 
 }
