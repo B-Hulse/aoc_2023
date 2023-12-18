76751d7d06ce00be9e1ae98f11ea296a31454e64
Ben Hulse <b.hulse99@gmail.com>
2023-12-18 16:59:34 +0000

    Switch Day 12 to DP

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -1,12 +1,10 @@
-#![allow(dead_code, unreachable_code)]
+use std::str::FromStr;
 
-use std::{collections::HashMap, str::FromStr};
-
-const INPUT: &str = include_str!("input/day12_example.txt");
+const INPUT: &str = include_str!("input/day12.txt");
 
 struct Puzzle {
-    springs: String,
-    blocks: Vec<i64>,
+    springs: Vec<u8>,
+    blocks: Vec<usize>,
 }
 
 impl FromStr for Puzzle {
@@ -19,38 +17,23 @@ impl FromStr for Puzzle {
 
         let blocks = blocks_str
             .split(",")
-            .filter_map(|count_str| count_str.parse::<i64>().ok())
+            .filter_map(|count_str| count_str.parse::<usize>().ok())
             .collect();
         Ok(Puzzle {
-            springs: springs.to_string(),
+            springs: springs.as_bytes().to_vec(),
             blocks,
         })
     }
 }
 
-fn remove_block_from_vals(
-    vals: &mut Vec<i64>,
-    block_size: i64,
-) -> bool {
-    if let Some(v) = vals.first() {
-        if block_size != *v {
-            return false;
-        }
-        vals.remove(0);
-    } else {
-        return false;
-    }
-    true
-}
-
 impl Puzzle {
     fn unfold(&self) -> Puzzle {
-        let mut new_springs = String::new();
+        let mut new_springs = Vec::new();
         let mut new_blocks = Vec::new();
 
         for _ in 0..5 {
-            new_springs.push_str(&self.springs);
-            new_springs.push('?');
+            new_springs.extend(self.springs.clone());
+            new_springs.push(b'?');
             new_blocks.append(&mut self.blocks.clone());
         }
 
@@ -62,135 +45,91 @@ impl Puzzle {
         };
     }
 
-    fn recursive_count_valid_permutations(
-        &self,
-        solved_cache: &mut HashMap<(usize, i64), i64>,
-        current_spring: String,
-        solved_block_count: i64,
-    ) -> i64 {
-        let unknown_count = current_spring.chars().filter(|c| *c == '?').count();
+    fn solve_dp(&self) -> i64 {
+        let mut springs = self.springs.clone();
+        springs.push(b'.');
 
-        if unknown_count == 0 {
-            return if self.is_valid(&current_spring) {
-                1
-            } else {
-                0
-            };
+        let mut dp_table = vec![vec![0;springs.len()]; self.blocks.len()];
+    
+        // For each i in springs, max_hash_for_i[i] is the number of # and ? before i.
+        let mut sum = 0;
+
+        let mut max_hash_for_i = springs.iter().map(|&b| {
+            if b != b'.' {
+                sum += 1;
+            }
+            sum
+        }).collect::<Vec<_>>();
+        max_hash_for_i.insert(0, 0);
+        let max_hash_for_i = max_hash_for_i;
+    
+        //  How many dots will there be in a valid solution
+        let wiggle_room = springs.len() - self.blocks.iter().sum::<usize>() - self.blocks.len() + 1;
+    
+        // Count combinations, handling the first row as a special case.
+        let block_0_width = self.blocks[0];
+        let mut combination_count:i64 = 0;
+        let mut valid = true;
+    
+        for starting_i in 0..wiggle_room {
+            // In order to be a broken spring, an interval must only contains `#` or `?`
+            // characters and not have a '#' character immediately before or after.
+            if springs[starting_i + block_0_width] == b'#' {
+                combination_count = 0;
+            } else if valid && max_hash_for_i[starting_i + block_0_width] - max_hash_for_i[starting_i] == block_0_width {
+                combination_count += 1;
+            }
+    
+            dp_table[0][starting_i + block_0_width] = combination_count;
+    
+            // All following patters are invalid if our window is preceded by a '#'.
+            valid &= springs[starting_i] != b'#';
         }
-
-        if let Some(permutations) = solved_cache.get(&(unknown_count, solved_block_count)) {
-            return *permutations;
-        }
-
-        let mut permutations = 0;
-
-        // Dot
-        let dot_input = current_spring.replacen('?', ".", 1);
-        if let Some(dot_solved_block_count) = self.count_solved_blocks(&dot_input) {
-            permutations += self.recursive_count_valid_permutations(
-                solved_cache,
-                dot_input,
-                dot_solved_block_count,
-            );
-        }
-
-        // Hash
-        let hash_input = current_spring.replacen('?', "#", 1);
-        if let Some(hash_solved_block_count) = self.count_solved_blocks(&hash_input) {
-            permutations += self.recursive_count_valid_permutations(
-                solved_cache,
-                hash_input,
-                hash_solved_block_count,
-            );
-        }
-
-        solved_cache.insert((unknown_count, solved_block_count), permutations);
-
-        permutations
-    }
-
-    fn count_solved_blocks(&self, input: &String) -> Option<i64> {
-        let mut filled = input.chars().peekable();
-        let mut vals = self.blocks.clone();
-
-        let mut solved_block_count = 0;
-
-        let mut current_block_size = 0;
-        while let Some(c) = filled.next() {
-            if c == '#' {
-                current_block_size += 1;
-
-                let next = filled.peek();
-
-                if next.is_none() || *next.unwrap() == '.' {
-                    if remove_block_from_vals(&mut vals, current_block_size) {
-                        solved_block_count += 1;
-                    }
-                    else {
-                        return None;
-                    }
-                    current_block_size = 0;
+    
+        let mut next_i = block_0_width + 1;
+    
+        for (block_i, &block_size) in self.blocks.iter().enumerate().skip(1) {
+            let mut combination_count = 0;
+    
+            for i in next_i..next_i + wiggle_room {
+                if springs[i + block_size] == b'#' {
+                    combination_count = 0;
+                } else if dp_table[block_i - 1][i - 1] > 0
+                    && springs[i - 1] != b'#'
+                    && max_hash_for_i[i + block_size] - max_hash_for_i[i] == block_size
+                {
+                    combination_count += dp_table[block_i - 1][i - 1];
                 }
+    
+                dp_table[block_i][i + block_size] = combination_count;
             }
-            else if c == '?' {
-                break;
-            }
+    
+            next_i += block_size + 1;
         }
-
-        Some(solved_block_count)
-    }
-
-    fn is_valid(&self, input: &String) -> bool {
-        let mut filled = input.chars().peekable();
-        let mut vals = self.blocks.clone();
-
-        let mut current_block_size = 0;
-        while let Some(c) = filled.next() {
-            if c == '#' {
-                current_block_size += 1;
-
-                let next = filled.peek();
-
-                if next.is_none() || *next.unwrap() == '.' {
-                    if !remove_block_from_vals(&mut vals, current_block_size) {
-                        return false;
-                    }
-                    current_block_size = 0;
-                }
-            }
-        }
-
-        if vals.is_empty() {
-            return true;
-        }
-        return false;
+    
+        *dp_table.last().unwrap().last().unwrap()
     }
 }
 
 pub fn part1() -> i64 {
-    // TODO: This is not working yet
-    return 0;
-    
-    INPUT
-        .lines()
-        .filter_map(|l| Puzzle::from_str(l).ok())
-        .map(|puzzle| {
-            puzzle.recursive_count_valid_permutations(&mut HashMap::new(), puzzle.springs.clone(), 0)
-        })
-        .sum::<i64>()
+    let puzzles : Vec<_> = INPUT.lines().filter_map(|l| {
+        Puzzle::from_str(l).ok()
+    })
+    .collect();
+
+    puzzles.into_iter().map(|p| p.solve_dp()).sum::<i64>()
 }
 
 pub fn part2() -> i64 {
-    // TODO: This is not working yet
-    return 0;
+    let puzzles : Vec<_> = INPUT.lines().filter_map(|l| {
+        if let Ok(p) = Puzzle::from_str(l) {
+            Some(p.unfold())
+        }
+        else {
+            None
+        }
+    })
+    .collect();
 
-    INPUT
-        .lines()
-        .filter_map(|l| Puzzle::from_str(l).ok())
-        .map(|puzzle| puzzle.unfold())
-        .map(|p| {
-            let temp = p.recursive_count_valid_permutations(&mut HashMap::new(), p.springs.clone(), 0);
-            temp
-        })
-        .sum::<i64>()
+    puzzles.into_iter().map(|p| p.solve_dp()).sum::<i64>()
 }
